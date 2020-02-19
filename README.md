e45b9d512e04df44c69b2081822e394958037df2
eTarget1 <louisclifford10@yahoo.fr>
2020-02-19 15:56:44 -0500

    Set optionsProfile using firefox webdriver

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # driverConfigSelenium
 
 link to follow: https://www.selenium.dev/selenium-ide/docs/en/introduction/getting-started
+
+On ubuntu, we can use: firefox --help command line to describe firefox command that available on the OS.
+
+To create your profile:
+firefox -P newprofile
+
+You cannot copy this path while creating profile, but when you go to path with profiles:  ~/.mozilla/firefox/
--- index.js
+++ index.js
@@ -2,12 +2,8 @@ const { Builder } = require("selenium-webdriver");
 const firefox = require("selenium-webdriver/firefox");
 const options = new firefox.Options();
 
-options.setPreference("browser.download.dir", "C:\\mySeleniumDownload");
-options.setPreference("browser.download.folderList", 2)
-options.setPreference("browser.helpersApps.neverAsk.saveToDisk", "application/x-csv");
 
-// const driver = new Builder().forBrowser("firefox").build();
-// driver.get("http://facebook.com");       
+options.setProfile = '/home/noukod/.mozilla/firefox/SeleniumTutorial'
 
 const driver = new Builder().forBrowser("firefox").setFirefoxOptions(options).build();
-driver.get("http://samplecsv.s3.amazonaws.com/Sacramentorealestatetransactions.csv");
+driver.get("http://google.com");     
