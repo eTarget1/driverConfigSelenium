b0e77f66b4eb2648e200eda72cf12c7458ddc647
eTarget1 <louisclifford10@yahoo.fr>
2020-02-19 17:48:13 -0500

    Created proxy with selenium-webdriver

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const { Builder } = require("selenium-webdriver");
 const firefox = require("selenium-webdriver/firefox");
-const options = new firefox.Options();
+const proxy = require("selenium-webdriver/proxy");
 
+const options = new firefox.Options();
 
 options.setProfile = '/home/noukod/.mozilla/firefox/SeleniumTutorial'
 
-const driver = new Builder().forBrowser("firefox").setFirefoxOptions(options).build();
-driver.get("http://google.com");     
+const proxyServer = "136.25.2.43:40614";
+
+
+const driver = new Builder()
+  .forBrowser("firefox")
+  .setFirefoxOptions(options)
+  .setProxy(proxy.manual({
+    http: proxyServer,
+    https: proxyServer
+  }))
+  .build();
+
+driver.get("https://whatismyipaddress.com/");     
