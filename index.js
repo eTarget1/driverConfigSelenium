e373e7aa2b06d5e996200ec9c21c5a033d9ac1c3
eTarget1 <louisclifford10@yahoo.fr>
2020-02-19 14:37:51 -0500

    Set browser to setPreference method with selenium

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 const { Builder } = require("selenium-webdriver");
+const firefox = require("selenium-webdriver/firefox");
+const options = new firefox.Options();
 
-const driver = new Builder().forBrowser("firefox").build();
-driver.get("http://facebook.com");
+options.setPreference("browser.download.dir", "C:\\mySeleniumDownload");
+options.setPreference("browser.download.folderList", 2)
+options.setPreference("browser.helpersApps.neverAsk.saveToDisk", "application/x-csv");
+
+// const driver = new Builder().forBrowser("firefox").build();
+// driver.get("http://facebook.com");       
+
+const driver = new Builder().forBrowser("firefox").setFirefoxOptions(options).build();
+driver.get("http://samplecsv.s3.amazonaws.com/Sacramentorealestatetransactions.csv");
