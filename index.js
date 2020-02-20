64c94afe44d0f375a4bd84f86dcdc3cede30a2ca
eTarget1 <louisclifford10@yahoo.fr>
2020-02-20 09:53:25 -0500

    Added how to wait for task s to finish with selenium-webdriver

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
 const { Builder } = require("selenium-webdriver");
-const firefox = require("selenium-webdriver/firefox");
-const proxy = require("selenium-webdriver/proxy");
-
-const options = new firefox.Options();
-
-options.setProfile = '/home/noukod/.mozilla/firefox/SeleniumTutorial'
-
-const proxyServer = "136.25.2.43:40614";
 
 
 const driver = new Builder()
   .forBrowser("firefox")
-  .setFirefoxOptions(options)
-  .setProxy(proxy.manual({
-    http: proxyServer,
-    https: proxyServer
-  }))
+
   .build();
 
-driver.get("https://whatismyipaddress.com/");     
+
+async function openWebsites(params) {
+  try {
+    await (await driver).get("http://yahoo.com");
+    await (await driver).get("http://google.com");
+  } catch (error) {
+    console.log(error);
+  }
+
+}
+
+openWebsites()
